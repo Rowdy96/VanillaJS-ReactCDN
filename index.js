a2906ca8baeaab5b60baa1f093ccf94689206931
Rahul Mondal <rahul.arambagh96@gmail.com>
2023-02-25 19:41:04 +0530

    React with no build tooling

---
+++ index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+function LikeButton(props) {
+  const [liked, setLiked] = React.useState(false);
+
+  if (liked) {
+    return "You liked comment number " + props.commentID;
+  }
+
+  return React.createElement(
+    "button",
+    { onClick: () => setLiked(true) },
+    "Like"
+  );
+}
+
+// Find all DOM containers, and render Like buttons into them.
+// Note we're using CSS classes instead of IDs so that we can find several nodes.
+document.querySelectorAll(".like-button").forEach((rootNode) => {
+  const root = ReactDOM.createRoot(rootNode);
+  // Read the comment ID from a data-* attribute.
+  const commentID = parseInt(rootNode.dataset.commentid);
+  root.render(React.createElement(LikeButton, { commentID: commentID }));
+});
